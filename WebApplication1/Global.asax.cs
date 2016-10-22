266025d50b524d120e992e97cd30f379cf2c61a6
Franz <Franz@FRANZPC>
2016-10-22 16:45:56 +0200

    added api added

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using WebApplication1.App_Start;
 
 namespace WebApplication1
 {
@@ -12,6 +14,7 @@ namespace WebApplication1
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
